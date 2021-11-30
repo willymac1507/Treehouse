414745772e81b4fc631df7aa7aaa353f0a76f77b
Will McCloy <william.mccloy@googlemail.com>
2021-11-30 11:27:42 +0000

    Initial commit

---
+++ decoder.rb
@@ -0,0 +1,30 @@
+KEY = {
+  1 => 'A',
+  2 => 'B',
+  3 => 'C',
+  4 => 'D',
+  5 => 'E',
+  6 => 'F',
+  7 => 'G',
+  8 => 'H',
+  9 => 'I',
+  10 => 'J',
+  11 => 'K',
+  25 => 'Y',
+  26 => 'Z',
+}
+
+# Define a method that takes an array of numbers to decode.
+def decode(numbers)
+  text = ""
+  # Process each number in the array
+  numbers.each do |number|
+    # Get the letter that corresponds to this number.
+    letter = KEY[number]
+    # Add it onto the string.
+    If letter != nil
+      text += letter
+  end
+  # Return the decoded string.
+  return text
+end
